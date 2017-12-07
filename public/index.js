7812454454c832448756bca5c014b21823b78f22
Parker E.F. Okonek <okonekp@oregonstate.edu>
2017-12-06 22:49:22 -0800

    Login Modal pops up and is integrated

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
-//Empty JS file
+function showLoginModal() {
+	var modalBackdrop = document.getElementById('modal-backdrop');
+	var loginModal = document.getElementById('login-modal');
+
+	modalBackdrop.classList.remove('hidden');
+	loginModal.classList.remove('hidden');
+}
+
+var loginButton = document.getElementById('login');
+if (loginButton) {
+		loginButton.addEventListener('click',showLoginModal);
+}
