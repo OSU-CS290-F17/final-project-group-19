e669a8385a0c6fd9a77105b9277c0fbbbbd502b2
Parker E.F. Okonek <okonekp@oregonstate.edu>
2017-12-07 20:16:25 -0800

    Added basic MongoDB to server

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
+//Mongo DB specific things
+var MongoClient = require('mongodb').MongoClient;
+var mongoHost = 'classmongo.engr.oregonstate.edu';//process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = 'cs290_okonekp';//process.env.MONGO_USER;
+var mongoPassword = 'cs290_okonekp';//process.env.MONGO_PASSWORD;
+var mongoDBName = 'cs290_okonekp';//process.env.MONGO_DB;
+
+var mongoURL = 'mongodb://'+mongoUser+':'+mongoPassword+'@'+mongoHost+':'+mongoPort+'/'+mongoDBName;
+
 var app = express();
 var port = process.env.PORT || 3000;
 var characterData = require('./characterData.json');
 var userData = require('./userData.json');
-var newsData = require('./newsData.json');
 
 app.use(express.static('public'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -17,12 +26,16 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 
 app.get('/', function(req, res, next){
+	var newsCollection = mongoDBDatabase.collection('newsCards');
+
+	newsCollection.find({}).toArray(function (err, results) {
 		res.status(200).render('mainPage', {
         loggedId: 0,
-        newsData: newsData
-    });
+        newsData: results
+    	});
+	});
 });
-
+	
 app.get('/:username/:password', function(req, res, next){
     var username = req.params.username;
     var password = req.params.password;
@@ -40,6 +53,13 @@ app.get('*', function (req, res) {
     res.status(404).render('404');
 });
 
-app.listen(port, function () {
-    console.log("== Server listening on port", port);
+MongoClient.connect(mongoURL, function (err, db) {
+	if (err) {
+		throw err;
+	}
+	mongoDBDatabase = db;
+	
+	app.listen(port, function () {
+	    console.log("== Server listening on port", port);
+	});
 });
